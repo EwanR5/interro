d5ab8258ee4433507c47488c80929af9e92f857f
EwanR5 <91669749+EwanR5@users.noreply.github.com>
2023-04-18 10:07:44 +0200

    Add files via upload

---
+++ IMATP5_Ramsamy_Ewan/IMATP5_Ramsamy_Ewan/Outils.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IMATP5_Ramsamy_Ewan
+{
+    public struct MethodesDuProjet
+    {
+        public void RemplissageTableau(ref int[,] tableau)
+        {
+            tableau[0, 0] = 1;
+
+            for (int i = 1; i < tableau.GetLength(0) - 1; i++)
+            {
+                for (int j = 0; j < tableau.GetLength(1) - 1; j++)
+                {
+                    if (j == 0)
+                    {
+                        tableau[i, j] = 1;
+                    }
+                    else
+                    {
+                        tableau[i, j] = tableau[i - 1, j - 1] + tableau[i - 1, j];
+                    }
+                }
+            }
+        }
+        public void AfficherTableau(int[,] tableau, int tailleMatrice, out string affichage)
+        {
+            affichage = "a" + ^tailleMatrice;
+            int exposantA = tailleMatrice - 1;
+            int exposantB = 1;
+
+            for (int j = 0; j < tableau.GetLength(1) - 2; j++)
+            {
+                affichage = affichage + tableau[tailleMatrice, j] + "a" + ^exposantA + "b" + ^exposantB;
+                exposantA = exposantA - 1;
+                exposantB = exposantB + 1;
+            }
+            affichage = affichage + "b" + ^exposantB;
+        }
+    }
+}
---
+++ IMATP5_Ramsamy_Ewan/IMATP5_Ramsamy_Ewan/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace IMATP5_Ramsamy_Ewan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MethodesDuProjet Outils = new MethodesDuProjet();
+
+            Console.WriteLine("Quel est le coefficient de votre opération ?");
+            int tailleMatrice = int.Parse(Console.ReadLine());
+
+            int[,] tableau = new int[tailleMatrice, tailleMatrice];
+            string affichage = "";
+
+            Outils.RemplissageTableau(ref tableau);
+            Outils.AfficherTableau(tableau, tailleMatrice, out affichage);
+
+            Console.WriteLine("Voici Votre opération : " + affichage);
+            Console.ReadLine();
+        }
+    }
+}
